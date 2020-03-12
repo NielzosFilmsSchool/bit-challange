f405d56b538f8190c68aacc5932feefc9a921f54
NielzosFilmsSchool <niels.hazelaar@student.rocva.nl>
2020-03-12 11:27:30 +0100

    final merging fixes

--- create_post.php
+++ create_post.php
@@ -1,16 +1,44 @@
 <h1>create post page</h1>
 
 <form action="create_post.php" method="post">
-    <input type="file"><br>
-    <input type="text" placeholder="Text..."><br>
-    <input type="submit">
+    <input type="file" name="file"><br>
+    <input type="text" name="title" placeholder="Titel"><br>
+    <input type="text" name="text" placeholder="Text..."><br>
+    <input type="submit" name="submit">
 </form>
 
 <?php
+$host = '127.0.0.1';
+$db   = 'bit_challange';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+?>
+
+<?php
 if(!isset($_COOKIE["logged_in"])){
-    header("Location: index.php");
+    header("Location: login.php");
 }
 
-
+if(isset($_POST["submit"])){
+    try {
+        $pdo = new PDO($dsn, $user, $pass, $options);
+        $stmt = $pdo->prepare(
+            "INSERT INTO post (title, text)
+            VALUES ('".$_POST["title"]."', '".$_POST["text"]."')"
+        );
+        $stmt->execute();
+        header("Location: index.php");
+    } catch (Exception $e) {
+        echo "<p>Error: ".$e->getMessage()."</p>";
+    }
+}
 
 ?>
--- index.php
+++ index.php
@@ -1,1 +1,63 @@
-<h1>index</h1>
+<link rel="stylesheet" type="text/css" href="style.css"></link>
+
+<h1>Post board</h1>
+
+<link rel="stylesheet" href="style.css">
+<link href='https://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+<div id='stars'></div>
+<div id='stars2'></div>
+<div id='stars3'></div>
+
+<div class="title">
+    <span>
+    Bit-Challange
+    </span>
+
+    <br>
+    <div id="names">
+    <span>
+      Powered by Niels, and Iz-Dine
+    </span>
+    </div>
+</div>
+
+<?php
+$host = '127.0.0.1';
+$db   = 'bit_challange';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+?>
+
+<?php
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+    $stmt = $pdo->query('SELECT * FROM post');
+    if($stmt->rowCount() == 0){
+        throw new Exception("Geen posts gevonden.");
+    }
+    ?>
+    <div class="post_container">
+    <?php
+    while($row = $stmt->fetch()) {
+        ?>
+        <div class="post">
+            <img src="" alt="Post image">
+            <h3><?= $row["title"]?></h3>
+        </div>
+        <?php
+    }
+    ?>
+    </div>
+    <?php
+} catch (Exception $e) {
+    echo "<p>Error: ".$e->getMessage()."</p>";
+}
+?>
